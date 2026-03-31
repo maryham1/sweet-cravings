64b4d632a8327028f56355e2bbe1f18c0fa759d9
maryham1 <maryham4real@gmail.com>
2026-03-31 10:10:57 +0100

    loading scroll

--- src/ui/Applayouts.jsx
+++ src/ui/Applayouts.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import { Outlet, useNavigation } from "react-router-dom";
 import Loading from "./Loading";
 
 function Applayouts() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
+
+  useEffect(() => {
+    if (isLoading) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    // Cleanup (important!)
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isLoading]);
+
   return (
     <>
       {isLoading && <Loading />}
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -6,36 +6,38 @@ import Testimonial from "../features/Pages/Testimonial";
 import Contact from "../features/Pages/Contact";
 function Home() {
   return (
-    <main>
-      <PageNav />
-      <section className="relative w-full h-[300px] overflow-hidden">
-        <video
-          className="absolute  left-0 w-full h-full  object-cover"
-          autoPlay
-          loop
-          muted
-          playsInline
-        >
-          <source src="\video\dessert.mp4" />
-        </video>
-        <div className="absolute inset-0 bg-black/50"></div>
-        <div className="relative z-10 flex flex-col items-center justify-center text-center gap-3 text-white h-full px-4">
-          <h1 className="text-3xl font-poppins">
-            Welcome to <span className="font-logo text-pink-600">Dessert</span>{" "}
-            Heaven
-          </h1>
-          <p>Every bite is a little celebration of sweetness</p>
-          <button className="bg-red-100 rounded-lg px-4 py-2.5 text-center w-[150px]">
-            <Link to="/menu" className="text-amber-950 font-poppins">
-              Explore menu
-            </Link>
-          </button>
-        </div>
-      </section>
-      <About />
-      <Testimonial />
+    <>
+      <main>
+        <PageNav />
+        <section className="relative w-full h-[300px] overflow-hidden">
+          <video
+            className="absolute  left-0 w-full h-full  object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+          >
+            <source src="\video\dessert.mp4" />
+          </video>
+          <div className="absolute inset-0 bg-black/50"></div>
+          <div className="relative z-10 flex flex-col items-center justify-center text-center gap-3 text-white h-full px-4">
+            <h1 className="text-3xl font-poppins">
+              Welcome to{" "}
+              <span className="font-logo text-pink-600">Dessert</span> Heaven
+            </h1>
+            <p>Every bite is a little celebration of sweetness</p>
+            <button className="bg-red-100 rounded-lg px-4 py-2.5 text-center w-[150px]">
+              <Link to="/menu" className="text-amber-950 font-poppins">
+                Explore menu
+              </Link>
+            </button>
+          </div>
+        </section>
+        <About />
+        <Testimonial />
+      </main>
       <Contact />
-    </main>
+    </>
   );
 }
 
